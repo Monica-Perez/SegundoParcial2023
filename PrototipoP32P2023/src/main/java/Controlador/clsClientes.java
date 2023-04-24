402db832574bce2a342cc4a191eb13493d9c6976
Monica Perez <mperezv32@miumg.edu.gt>
2023-04-24 12:26:42 -0600

    Subiendo Main con cls

---
+++ PrototipoP32P2023/src/main/java/Controlador/clsClientes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Usuario
+ */
+public class clsClientes {
+    private int IdCliente;
+    private String NombreCliente;
+    private String NitCliente;
+    private String DebeCliente;
+    private String HaberCliente;
+}
